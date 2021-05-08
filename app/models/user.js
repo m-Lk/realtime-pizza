a58206e6b01b235d44f4e0bc0438cdeb39a921f5
MK <mukesh.kalojiya55@gmail.com>
2021-05-08 15:27:54 +0530

    Auth system

---
+++ app/models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        default: 'Customer'
+    }
+}, {timestamps: true});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const flash = require('express-flash');
 const MongoDbStore = require('connect-mongo');
+const passport = require('passport');
 //new MongoDbStore(session);
 
 const app = express();
@@ -46,17 +47,27 @@ app.use(session({
     cookie: { maxAge: 1000 * 60 * 60 * 24}
 }));
 
+
+// passport config
+const passportInit = require('./app/config/passport');
+passportInit(passport);
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(flash());
 
 // Assets
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
 
 // Global middlewares
 app.use((req, res, next) => {
     res.locals.session = req.session;
+    res.locals.user = req.user;
     next();
-})
+});
 
 
 //set template engine
